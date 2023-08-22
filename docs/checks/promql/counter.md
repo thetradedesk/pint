1bba680764c5eb1dd11e4ac8033f2912e262fd8d
caryjpaepen <42157570+caryjpaepen@users.noreply.github.com>
2023-08-23 09:02:38 +1000

    Add promql/counter check (#5)

---
+++ docs/checks/promql/counter.md
@@ -0,0 +1,91 @@
+---
+layout: default
+parent: Checks
+grand_parent: Documentation
+---
+
+# promql/counter
+
+This check inspects counter metrics used in queries to verify that the raw counter value is not used in calculations.
+
+A counter should be wrapped by one of the following functions:
+
+- `rate`, `irate`, `increase` - [performing rate calculations while handling counter resets](https://promlabs.com/blog/2021/01/29/how-exactly-does-promql-calculate-rates/)
+  - Note: [`irate` is excluded from alerting rules](https://www.robustperception.io/avoid-irate-in-alerts/).  
+- `count`, `count_over_time`, `absent`, `absent_over_time`, `present_over_time` - these are common and valid use cases of any raw metric
+
+## Common problems
+
+### False Alarm
+
+While this check is useful in identifying misuse of raw counters, we recognize there are rare but valid use cases.
+
+Some examples:
+
+- a recording rule that evaluates a raw counter with label matchers (e.g. for filtering a subset of high cardinality metrics)
+- an alert that uses a counter as an info metric to join labels
+
+As a result, the check severity is set to `Warning` rather than `Bug`.
+
+### Metadata mismatch
+
+See [Same section in promql/rate check documentation](rate.md#metadata%20mismatch)
+
+## Configuration
+
+This check doesn't have any configuration options.
+
+## How to enable it
+
+This check is enabled by default for all configured Prometheus servers.
+
+## How to disable it
+
+You can disable this check globally by adding this config block:
+
+```js
+checks {
+  disabled = ["promql/counter"]
+}
+```
+
+You can also disable it for all rules inside given file by adding
+a comment anywhere in that file. Example:
+
+```yaml
+# pint file/disable promql/counter
+```
+
+Or you can disable it per rule by adding a comment to it. Example:
+
+```yaml
+# pint disable promql/counter
+```
+
+If you want to disable only individual instances of this check
+you can add a more specific comment.
+
+```yaml
+# pint disable promql/counter($prometheus)
+```
+
+Where `$prometheus` is the name of Prometheus server to disable.
+
+Example:
+
+```yaml
+# pint disable promql/counter(prod)
+```
+
+## How to snooze it
+
+You can disable this check until given time by adding a comment to it. Example:
+
+```yaml
+# pint snooze $TIMESTAMP promql/counter
+```
+
+Where `$TIMESTAMP` is either use [RFC3339](https://www.rfc-editor.org/rfc/rfc3339)
+formatted  or `YYYY-MM-DD`.
+Adding this comment will disable `promql/counter` *until* `$TIMESTAMP`, after that
+check will be re-enabled.
---
+++ internal/checks/promql_counter.go
@@ -0,0 +1,145 @@
+package checks
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	promParser "github.com/prometheus/prometheus/promql/parser"
+
+	"github.com/cloudflare/pint/internal/discovery"
+	"github.com/cloudflare/pint/internal/parser"
+	"github.com/cloudflare/pint/internal/promapi"
+)
+
+const (
+	CounterCheckName = "promql/counter"
+)
+
+var (
+	AllowedCounterFuncsForAlerts         = []string{"rate", "increase", "absent", "absent_over_time", "count", "count_over_time", "present_over_time"}
+	AllowedCounterFuncsForRecordingRules = append(AllowedCounterFuncsForAlerts, "irate")
+)
+
+func NewCounterCheck(prom *promapi.FailoverGroup) CounterCheck {
+	return CounterCheck{prom: prom}
+}
+
+type CounterCheck struct {
+	prom *promapi.FailoverGroup
+}
+
+func (c CounterCheck) Meta() CheckMeta {
+	return CheckMeta{IsOnline: true}
+}
+
+func (c CounterCheck) String() string {
+	return fmt.Sprintf("%s(%s)", CounterCheckName, c.prom.Name())
+}
+
+func (c CounterCheck) Reporter() string {
+	return CounterCheckName
+}
+
+func (c CounterCheck) Check(ctx context.Context, _ string, rule parser.Rule, entries []discovery.Entry) (problems []Problem) {
+	expr := rule.Expr()
+
+	if expr.SyntaxError != nil || (rule.AlertingRule == nil && rule.RecordingRule == nil) {
+		return problems
+	}
+
+	isAlertRule := rule.AlertingRule != nil
+
+	isCounterMap := &IsCounterMapForCounterCheck{
+		values: make(map[string]bool),
+	}
+	for _, problem := range c.checkNode(ctx, expr.Query, entries, false, isAlertRule, isCounterMap) {
+		problems = append(problems, Problem{
+			Fragment: problem.expr,
+			Lines:    expr.Lines(),
+			Reporter: c.Reporter(),
+			Text:     problem.text,
+			Severity: problem.severity,
+		})
+	}
+
+	return problems
+}
+
+func (c CounterCheck) checkNode(ctx context.Context, node *parser.PromQLNode, entries []discovery.Entry, parentUsesAllowedFunction, isAlertRule bool, isCounterMap *IsCounterMapForCounterCheck) (problems []exprProblem) {
+	allowedFuncs := AllowedCounterFuncsForRecordingRules
+	if isAlertRule {
+		allowedFuncs = AllowedCounterFuncsForAlerts
+	}
+
+	if s, ok := node.Node.(*promParser.VectorSelector); ok {
+		isCounter, ok := isCounterMap.values[s.Name]
+		if ok {
+			if !isCounter {
+				return problems
+			}
+		} else {
+			metadata, err := c.prom.Metadata(ctx, s.Name)
+			if err != nil {
+				text, severity := textAndSeverityFromError(err, c.Reporter(), c.prom.Name(), Bug)
+				problems = append(problems, exprProblem{
+					expr:     s.Name,
+					text:     text,
+					severity: severity,
+				})
+				return problems
+			}
+
+			isCounter := false
+
+			for _, m := range metadata.Metadata {
+				if m.Type == v1.MetricTypeCounter {
+					isCounter = true
+					break // exit the loop as soon as you find a counter
+				}
+			}
+
+			isCounterMap.values[s.Name] = isCounter
+		}
+		if isCounterMap.values[s.Name] && !parentUsesAllowedFunction {
+			allowedFuncString := "`" + strings.Join(allowedFuncs, "`, `") + "`"
+
+			p := exprProblem{
+				expr: node.Expr,
+				text: fmt.Sprintf("Counter metric `%s` should be used with one of following functions: (%s).", s.Name, allowedFuncString),
+				// There can be valid edge cases like a recording rule: `foo{label="value"}` or being constrained to use a counter as an info metric for joining.
+				severity: Warning,
+			}
+			problems = append(problems, p)
+		}
+	}
+
+	// Matrix wraps a single vector, we will retain `parentUsesAllowedFunction` value. (e.g. rate(x) or rate(x[2m]) are treated equally)
+	if _, ok := node.Node.(*promParser.MatrixSelector); !ok {
+		parentUsesAllowedFunction = false
+
+		if n, ok := node.Node.(*promParser.Call); ok && contains(allowedFuncs, n.Func.Name) {
+			parentUsesAllowedFunction = true
+		}
+	}
+
+	for _, child := range node.Children {
+		problems = append(problems, c.checkNode(ctx, child, entries, parentUsesAllowedFunction, isAlertRule, isCounterMap)...)
+	}
+
+	return problems
+}
+
+func contains(slice []string, value string) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+type IsCounterMapForCounterCheck struct {
+	values map[string]bool
+}
